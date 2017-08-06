7eabf434ef2475a9a5787d9b2dbfb2900a0397c9
Haider Ismail <haider@jaskcreative.co.uk>
2017-08-06 23:47:19 +0100

    fixed header nav

--- src/js/main.js
+++ src/js/main.js
@@ -87,6 +87,22 @@ $(document).ready(function() {
     });
   })();
 
+
+  //smooth scroll function (taken from online)
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+    {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   //Made with <3 for McCann
 
 
